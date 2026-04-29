ff3eb82af4edfa88ccbac765a38121aea6da3eea
HATheekshana <h.a.supuntheekshana@gmail.com>
2026-04-30 05:14:49 +0530

    lauma

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 from dotenv import load_dotenv
 from data.characters import characters5
 load_dotenv()
-CURRENT_RATE_UP_KEY = "raiden-shogun"
-CURRENT_RATE_UP_NAME = characters5.get(CURRENT_RATE_UP_KEY, "Raiden Shogun")
+CURRENT_RATE_UP_KEY = "lauma"
+CURRENT_RATE_UP_NAME = characters5.get(CURRENT_RATE_UP_KEY, "Lauma")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URL = os.getenv("MONGO_URL")
 ADMIN_VAL = os.getenv("ADMIN_ID")
